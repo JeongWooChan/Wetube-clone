b1360dc1df2a864c8c67099b479f2b50acf17dc3
정우찬 <vdncks25@naver.com>
2019-04-01 17:28:01 +0900

    code : Make my first express server
    매우 간단한 express server 구축

--- code/index.js
+++ code/index.js
@@ -1,1 +1,10 @@
-console.log('hi');
+const express = require('express'); // require는 node module을 가져오는 역할을 함 
+const app = express();  
+
+const PORT = 4000;
+
+function handleListening() {
+    console.log(`Listening on : http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
