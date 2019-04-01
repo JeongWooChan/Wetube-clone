249c6a29b6e745b14a2786a0cd60f7ae31c14de9
정우찬 <vdncks25@naver.com>
2019-04-01 22:16:07 +0900

    code : Study Router
    router에 대한 실습

--- code/app.js
+++ code/app.js
@@ -3,12 +3,8 @@ import morgan from 'morgan';
 import helmet from 'helmet'; // node.js 보안을 위한 미들웨어
 import cookieParser from 'cookie-parser'; 
 import bodyParser from 'body-parser'; 
-
+import { userRouter } from "./router";
 const app = express();  
-
-const PORT = 4000;
-
-const handleListening = () => { console.log(`Listening on : http://localhost:${PORT}`); }
 
 const handleHome = (req, res) => res.send('Hello from home!!');
 
@@ -20,15 +16,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(helmet());
 
-const betweenHome = (req, res, next) => {
-    console.log('Between'); 
-    next();  
-};
-
-app.use(betweenHome);
-
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter); // 누군가 /user 경로에 접속하면 이 router 전체를 사용하겠다. 
+
+export default app; 
