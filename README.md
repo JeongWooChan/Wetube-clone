d29cf75b108ee30e70571a1e652658822f300f27
정우찬 <vdncks25@naver.com>
2019-04-01 21:25:19 +0900

    code : Study middleware & install some middleware
    morgan, helmet, body-parser, cookie-parser 설치 
    middleware 실습

--- README.md
+++ README.md
@@ -40,6 +40,32 @@ npm install nodemon -D
 ```
 -D를 붙인 것은 해당 프로젝트가 필요한 것이 아니라 개발자가 필요로 한 것을 의미함 
 
+<br>
+
+## 5. Middleware 설치 
+### 5-1. morgan
+```bash
+npm install morgan
+```
+요청에 대한 정보를 콘솔에 기록해주는 미들웨어 
+
+### 5-2. helmet 
+```bash
+npm install helmet 
+```
+NodeJS의 보안을 위한 미들웨어 
+
+### 5-3. body-parser
+```bash
+npm install body-parser --save 
+```
+body로부터 정보를 얻을 수 있게 해주는 미들웨어
+
+### 5-4. cookie-parser 
+```bash
+npm install cookie-parser 
+```
+cookie에 유저 정보를 저장하기 위해 필요한 미들웨어 
 
 
 [공식홈페이지]:https://nodejs.org/ko/
--- code/index.js
+++ code/index.js
@@ -1,13 +1,31 @@
 import express from "express"; // require는 node module을 가져오는 역할을 함 
+import morgan from 'morgan';
+import helmet from 'helmet'; // node.js 보안을 위한 미들웨어
+import cookieParser from 'cookie-parser'; 
+import bodyParser from 'body-parser'; 
+
 const app = express();  
 
 const PORT = 4000;
 
 const handleListening = () => { console.log(`Listening on : http://localhost:${PORT}`); }
 
-const handleHome = (req, res) => s.   res.send('Hello from hone!!');
+const handleHome = (req, res) => res.send('Hello from home!!');
 
 const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(helmet());
+
+const betweenHome = (req, res, next) => {
+    console.log('Between'); 
+    next();  
+};
+
+app.use(betweenHome);
 
 app.get("/", handleHome);
 
