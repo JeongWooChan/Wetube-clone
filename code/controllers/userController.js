a0b0f0e5697073ab4058254261afed7f140a6090
정우찬 <vdncks25@naver.com>
2019-04-02 22:49:40 +0900

    code : Add a simple sign_up function in join controller
    join 에서 간단한 회원가입 기능 추가(post처리)

--- code/controllers/userController.js
+++ code/controllers/userController.js
@@ -1,4 +1,20 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join"}); 
+import routes from "../routes";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join"}); 
+export const postJoin = (req, res) => {
+    const {
+        body: {name, email, password, password2}
+    } = req;
+
+    if(password != password2) {
+        res.status(400); // 잘못된 요청임을 웹사이트가 알아들을 수 있는 status 코드 값으로 전달 
+        res.render("join", { pageTitle: "Join"});    
+    } else {
+        // To Do : Register user 
+        // To Do : Log user in 
+        res.redirect(routes.home);
+    }
+}
 export const login = (req, res) => res.render("login", { pageTitle: "Log In"}); 
 export const logout = (req, res) => res.render("logout", { pageTitle: "Log Out"}); 
 export const users = (req, res) => res.render("users", { pageTitle: "Search"});
