626c7f1def2d4fc4325f31c70aa5f8cd8a6abc6b
정우찬 <vdncks25@naver.com>
2019-04-02 17:08:08 +0900

    code : Use Pug
    간단하게 퍼그를 사용해 봄

--- code/app.js
+++ code/app.js
@@ -7,16 +7,18 @@ import routes from "./routes";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localMiddleware } from "./middlewares";
 const app = express();  
 
 app.set('view engine', "pug"); // 템플릿 엔진을 사용하기 위한 세팅 
 
+app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(helmet());
 
+app.use(localMiddleware);
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter); 
 app.use(routes.videos, videoRouter); 
