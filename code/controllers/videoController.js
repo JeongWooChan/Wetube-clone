e60a99650a64d032ef11b2ef7bcca03e64a9fc92
정우찬 <vdncks25@naver.com>
2019-04-04 16:55:57 +0900

    code : Add delete video function
    비디오 삭제 기능 구현

--- code/controllers/videoController.js
+++ code/controllers/videoController.js
@@ -40,7 +40,7 @@ export const videoDetail = async (req, res) => {
     // req.params.id
     try {
         const video = await Video.findById(id);
-        res.render("videoDetail", { pageTitle : "Video Detail", video});
+        res.render("videoDetail", { pageTitle : video.title, video});
     } catch(err) {
         console.log(err);
         res.redirect(routes.home);
@@ -69,4 +69,14 @@ export const postEditVideo = async (req, res) => {
         res.redirect(routes.home);
     }
 }
-export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delte Video" });
+export const deleteVideo = async (req, res) => {
+    const {
+        params: {id}
+    } = req; 
+    try {
+        await Video.findOneAndRemove({ _id: id }); 
+    } catch (err) {
+        console.log(err); 
+    }
+    res.redirect(routes.home);
+}
