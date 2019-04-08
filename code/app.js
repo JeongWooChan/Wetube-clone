5cd5a466dd04416fb0293315b396d5cbea648182
정우찬 <vdncks25@naver.com>
2019-04-08 21:10:41 +0900

    Store the express-session in the database.

--- code/app.js
+++ code/app.js
@@ -4,7 +4,9 @@ import helmet from 'helmet'; // node.js 보안을 위한 미들웨어
 import cookieParser from 'cookie-parser'; 
 import bodyParser from 'body-parser';
 import passport from "passport"; 
+import mongoose from "mongoose";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import routes from "./routes";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
@@ -13,6 +15,8 @@ import { localMiddleware } from "./middlewares";
 import "./passport";
 
 const app = express();  
+
+const CookieStore = MongoStore(session); 
 
 app.set('view engine', "pug"); // 템플릿 엔진을 사용하기 위한 세팅 
 
@@ -23,11 +27,14 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(session({
-    secret: process.env.COOKIE_SECRET, 
-    resave: true, 
-    saveUninitialized: false
-}))
+app.use(
+    session({
+        secret: process.env.COOKIE_SECRET,
+        resave: true,
+        saveUninitialized: false,
+        store: new CookieStore({ mongooseConnection: mongoose.connection })
+    })
+);
 app.use(passport.initialize());
 app.use(passport.session()); 
 
