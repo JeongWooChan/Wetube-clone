bcf5ba829d3881be54daf9bc39afa0d1b6d4055f
정우찬 <vdncks25@naver.com>
2019-04-10 15:24:33 +0900

    code : Make custom video player button with SCSS

--- code/controllers/videoController.js
+++ code/controllers/videoController.js
@@ -52,7 +52,6 @@ export const videoDetail = async (req, res) => {
     // req.params.id
     try {
         const video = await Video.findById(id).populate('creator');
-        console.log(video);
         res.render("videoDetail", { pageTitle : video.title, video});
     } catch(err) {
         console.log(err);
