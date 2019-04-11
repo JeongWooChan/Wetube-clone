3108738588765dd1120d20d40df895933e1dcbb8
정우찬 <vdncks25@naver.com>
2019-04-11 22:30:28 +0900

    review : #2.5 ES6 on NodeJS using Babel

--- review/index.js
+++ review/index.js
@@ -1,4 +1,5 @@
-const express = require("express");
+import express from "express"; 
+
 const app = express(); 
 
 const PORT = 4001; 
@@ -6,5 +7,17 @@ const PORT = 4001;
 const handleListening = () => {
     console.log(`Listening on : http://localhost:${PORT}`); 
 }
+
+const handleHome = (req, res) => {
+    res.send("Hi from home"); 
+}
+
+const handleProfile = (req, res) => {
+    res.send("Here is my profile page");
+}
+
+app.get("/", handleHome); 
+
+app.get("/profile", handleProfile); 
 
 app.listen(PORT, handleListening); 
