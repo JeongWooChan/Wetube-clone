ef6682f5a9ad564475a1ad61942b27258650140a
정우찬 <vdncks25@naver.com>
2019-04-14 22:51:29 +0900

    review : #3.7 Uploading and Creating a Video

--- review/app.js
+++ review/app.js
@@ -18,6 +18,7 @@ app.use(morgan("dev"));
 app.use(cookieParser()); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
+app.use("/uploads", express.static("uploads")); 
 
 app.use(localsMiddleware); 
 app.use(routes.home, globalRouter); 
