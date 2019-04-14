a5f7d7675dd655278967832f1b02bac7c8a0b89e
정우찬 <vdncks25@naver.com>
2019-04-15 00:03:08 +0900

    review : #3.9 Editing a Video

--- review/controllers/videoController.js
+++ review/controllers/videoController.js
@@ -1,5 +1,6 @@
 import routes from "../routes";
 import Video from "../models/Video"; 
+import { resolveSoa } from "dns";
 
 export const home = async (req, res) => {
     try {
@@ -36,16 +37,40 @@ export const postUpload = async (req, res) => {
 }
 
 export const videoDetail = async (req, res) => {
-    console.log(req.params.id);
     const {
         params: { id }
     } = req
-    const video = await Video.findById(id);
-    res.render("videoDetail", { pageTitle : "Video Detail", video});
+    try {
+        const video = await Video.findById(id);
+        res.render("videoDetail", { pageTitle : "Video Detail", video});
+    } catch (err) {
+        res.redirect(routes.home);
+    }
 }
 
-export const editVideo = (req, res) => {
-    res.render("editVideo", { pageTitle : "Edit Video"});
+export const getEditVideo = async (req, res) => {
+    const {
+        params : { id }
+    } = req 
+    try {
+        const video = await Video.findById(id);
+        res.render("editVideo", { pageTitle: `Edit ${video.title}`, video});
+    } catch(err) {
+        res.redirect(routes.home);
+    }
+}
+
+export const postEditVideo = async (req, res) => {
+    const { 
+        params : { id },
+        body : {title, description}
+    } = req 
+    try {
+        await Video.findByIdAndUpdate({_id : id}, {title, description}); 
+        res.redirect(routes.videoDetail(id));
+    } catch(err) {
+        res.redirect(routes.home);
+    }
 }
 
 export const deleteVideo = (req, res) => {
