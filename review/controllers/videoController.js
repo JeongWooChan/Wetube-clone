09394311b8b0eefd28529b853d7ab35561802d6b
정우찬 <vdncks25@naver.com>
2019-04-14 23:22:36 +0900

    review : #3.8 Getting Video by ID

--- review/controllers/videoController.js
+++ review/controllers/videoController.js
@@ -35,8 +35,13 @@ export const postUpload = async (req, res) => {
     res.redirect(routes.videoDetail(newVideo.id));
 }
 
-export const videoDetail = (req, res) => {
-    res.render("videoDetail", { pageTitle : "Video Detail"});
+export const videoDetail = async (req, res) => {
+    console.log(req.params.id);
+    const {
+        params: { id }
+    } = req
+    const video = await Video.findById(id);
+    res.render("videoDetail", { pageTitle : "Video Detail", video});
 }
 
 export const editVideo = (req, res) => {
