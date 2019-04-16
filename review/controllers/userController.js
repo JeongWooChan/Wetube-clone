e5ddae34d41c0487267ed73f13d3ea032b895af8
정우찬 <vdncks25@naver.com>
2019-04-16 17:10:49 +0900

    review : #7.2 Change Password

--- review/controllers/userController.js
+++ review/controllers/userController.js
@@ -67,10 +67,28 @@ export const postEditProfile = async (req, res) => {
         }); 
         res.redirect(routes.me);
     } catch (err) {
-        res.render("editProfile", { pageTitle : "Edit Profile" });   
+        res.redirect(routes.editProfile); 
     }
 }
 
-export const changePassword = (req, res) => {
+export const getChangePassword = (req, res) => {
     res.render("changePassword", { pageTitle : "Change Password" });
+}
+
+export const postChangePassword = async (req, res) => { 
+    const {
+        body : {oldPassword, newPassword, newPassword1}
+    } = req; 
+    try { 
+        if(newPassword !== newPassword1) {
+            res.status(400);
+            res.redirect(`/users/${routes.changePassword}`);
+        } else {
+            await req.user.changePassword(oldPassword, newPassword);
+            res.redirect(routes.me);
+        }
+    } catch (err) {
+        res.status(400); 
+        res.redirect(`/users/${routes.changePassword}`);
+    }
 }
