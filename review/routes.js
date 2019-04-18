2fb960661a89b323ad8cdc2966587f46c8cb5840
정우찬 <vdncks25@naver.com>
2019-04-18 15:41:17 +0900

    review : #10.4 API Adding a Comment

--- review/routes.js
+++ review/routes.js
@@ -22,6 +22,7 @@ const DELETE_VIDEO = "/:id/delete";
 // API 
 const API = "/api"; 
 const REGISTER_VIEW = "/:id/view";
+const ADD_COMMENT = "/:id/comment"
 
 const routes = {
     home: HOME,
@@ -64,7 +65,8 @@ const routes = {
     }, 
     me: ME, 
     api: API, 
-    register_view: REGISTER_VIEW
+    register_view: REGISTER_VIEW,
+    addComment: ADD_COMMENT
 }; 
 
 export default routes;
